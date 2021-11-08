9561c416b213bc5db32f5a5a8d8289239e75f746
Shivanisajjan269 <ssajjan1@asu.edu>
2021-11-07 17:32:58 -0700

    implementation of power operation

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -12,13 +12,8 @@ export default function operate(numberOne, numberTwo, operation) {
   if (operation === "x") {
     return one.times(two).toString();
   }
-  if (operation === "÷") {
-    if (two === "0") {
-      alert("Divide by 0 error");
-      return "0";
-    } else {
-      return one.div(two).toString();
-    }
+  if (operation === "x^y") {
+      return one.pow(Number(numberTwo)).toString();
   }
   throw Error(`Unknown operation '${operation}'`);
 }
